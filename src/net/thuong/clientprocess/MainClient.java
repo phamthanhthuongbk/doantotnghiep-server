c05f661127fedfdc282faa34f67fc56000e9c987
Pham Thanh Thuong <phamthanhthuongbk@gmail.com>
2015-05-17 14:38:18 +0700

    process with UI

--- src/net/thuong/clientprocess/MainClient.java
+++ src/net/thuong/clientprocess/MainClient.java
@@ -39,7 +39,7 @@ public class MainClient {
 		processThread.start();
 
 		sendUDPThread.addQueue(new BasePacketCustom(BasePacketCustom.PACKET_TYPE_HELLO, "thuong123".getBytes()));
-		sendUDPThread.addQueue(new BasePacketCustom(BasePacketCustom.PACKET_TYPE_SEND_STRING, "thuong".getBytes()));
+//		sendUDPThread.addQueue(new BasePacketCustom(BasePacketCustom.PACKET_TYPE_SEND_STRING, "thuong".getBytes()));
 
 		// clientSocket.close();
 	}
